09505f494f809e6a1f49bda3a1ad1c6012bbe855
Brian Perry <contact.brianperry@gmail.com>
2020-07-19 10:21:33 -0500

    Created base of initial container component

---
+++ vue-nes/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-nes/src/components/container/Container.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="nes-container with-title is-centered">
+    <h2 class="title"><a href="">Container Title</a></h2>
+    <p>This is a container</p>
+  </div>
+</template>
+
+<script>
+import "nes.css/css/nes.min.css";
+
+export default {
+
+}
+</script>
+
+<style scoped>
+  @import url("https://fonts.googleapis.com/css?family=Press+Start+2P");
+</style>
