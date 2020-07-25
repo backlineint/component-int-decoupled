fdc81df43b0d0e46881c5d8b4a0d180c78863f52
Brian Perry <contact.brianperry@gmail.com>
2020-07-25 10:17:57 -0500

    Created example using Component module

--- vue-nes/src/components/container/ContainerBlock.vue
+++ vue-nes/src/components/container/ContainerBlock.vue
@@ -26,13 +26,15 @@ export default {
     uuid: String
   },
   mounted () {
-    fetch(`http://decoupled-component-sandbox.lndo.site/jsonapi/node/game/${this.uuid}?include=field_image`)
+    const drupalProvider = document.querySelector(".nes_container");
+    const config = drupalProvider.dataset;
+    fetch(`http://decoupled-component-sandbox.lndo.site/jsonapi/node/game?filter[drupal_internal__nid]=${config.game}&include=field_image`)
       .then(response => response.json())
       .then(data => {
-        this.title = data.data.attributes.title
-        this.platform = data.data.attributes.field_platform
-        this.year = data.data.attributes.field_year.toString()
-        this.body = data.data.attributes.body.processed
+        this.title = data.data[0].attributes.title
+        this.platform = data.data[0].attributes.field_platform
+        this.year = data.data[0].attributes.field_year.toString()
+        this.body = data.data[0].attributes.body.processed
         this.image = `http://decoupled-component-sandbox.lndo.site${data.included[0].attributes.uri.url}`
       });
   },
